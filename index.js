825cb1657b95833e0c2e75171adba290fe160abb
NurFarahIzza <nurfarahizzati3@gmail.com>
2023-03-30 15:54:30 +0800

    messages

---
+++ index.js
@@ -0,0 +1,53 @@
+let message = "benr"
+//console.log(message, typeof message)
+
+message = 123
+//console.log(message, typeof message)
+
+//let str = "BENR"
+//let num = 3523
+
+//let course = str + num
+//console.log(course, typeof course)
+
+//let str = "1"
+//let num = 1
+
+//let total = parseInt(str) + num
+//console.log(total, typeof total)
+
+//let str = "1.258"
+//let num = 1
+
+//let total = parseFloat(str) + num
+//console.log(total, typeof total)
+
+//let data = ["benr", 3523, true]
+
+//console.log(data[0])
+//console.log(data[1])
+//console.log(data[2])
+//console.log(data[3])
+
+let user = {
+    name: "Farah",
+    age: 23,
+    faculty: "FKEKK"
+ }
+ console.log(user.name)
+ console.log(user.age)
+ console.log(user.faculty)
+
+let messages = [
+    {name: "Farah",
+    date: "6/7/2023",
+    msg: "good video",
+    like: 1000
+},
+    { name: "Awin",
+    date: "15/7/2023",
+    msg: "good dance",
+    lke: 3000
+,}
+]
+console.log(messages[1].msg) // >> good dance
